904e418f99f27092d3a3dac3847025edff7d14e7
Vicky <47.efilm.infobibliotecas@gmail.com>
2024-05-05 00:16:55 +0200

    page activities created

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import logo from './assets/img/irlakurri.png';
 import './App.css'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Activities from './pages/activities/activities.jsx'; // Ajusta la ruta según la ubicación real de tu archivo Activities.jsx
 
 function App() {
   return (
-    <div id="home-container">
-      <h1>IRLA-KURRI</h1>
-      <section>
-        <article className="logo-box">
-          {/* <img src={require('@/assets/img/irlakurri.png').default} alt="logo" /> */}
-          {/* <img src={require('@/assets/react.svg')} alt="logo" /> */}
-        </article>
-        <article className="login-box">
-          <div className="login-date">
-            <label>Izena:</label>
-            <input type="text" />
-          </div>
-          <div className="login-date">
-            <label>Pasahitza:</label>
-            <input className="input-internal" type="password" />
-          </div>
-          <div className="login-date">
-            <button>SARTU</button>
-          </div>
-        </article>
-      </section>
-    </div>
+    <Router>
+      <div id="home-container">
+        <h1>IRLA-KURRI</h1>
+        <section>
+          <article className="logo-box">
+            <img src={logo} alt="logo" />
+          </article>
+          <article className="login-box">
+            <div className="login-date">
+              <label>Izena:</label>
+              <input type="text" />
+            </div>
+            <div className="login-date">
+              <label>Pasahitza:</label>
+              <input className="input-internal" type="password" />
+            </div>
+            <div className="login-date">
+              {/* Utiliza el componente Link para dirigirte a la página de actividades */}
+              <Link to="/activities">
+                <button>SARTU</button>
+              </Link>
+            </div>
+          </article>
+        </section>
+
+        {/* Configura las rutas de la aplicación */}
+        <Switch>
+          <Route path="/activities">
+            <Activities />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-export default App
+
+export default App;
+
