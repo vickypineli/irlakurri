154957f8ff1f8e9a40ee76db1b64335c8205f003
Vicky <47.efilm.infobibliotecas@gmail.com>
2024-05-06 20:21:54 +0200

    word by word created

--- front/src/pages/wordByWord/wordByWord.jsx
+++ front/src/pages/wordByWord/wordByWord.jsx
@@ -1,17 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState,useEffect } from 'react';
+import './wordByWord.css';
 
 const WordByWord = () => {
-  const [word, setWord] = useState('');
-  const [wordsByMinute, setWordsByMinute] = useState(10);
-  const [textByWords, setTextByWords] = useState([]);
+
   const [texts, setTexts] = useState([]);
+  const [textByWords, setTextByWords] = useState([]);
+  const [wordsByMinute, setWordsByMinute] = useState(10);
+  const [word, setWord] = useState('');
   const [textSelected, setTextSelected] = useState('');
-  const [fontSelected, setFontSelected] = useState('');
-
-
+  const  fonts =
   useEffect(() => {
     loadData();
-  }, []);
+     console.log ('word by word loaded');
+     console.log ('texts',texts);
+
+   }, []);
+
 
   const loadData = async () => {
     try {
@@ -20,78 +24,40 @@ const WordByWord = () => {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
+      console.log('data',data)
       setTexts(data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-
-  const pauseText = () => {
-    clearInterval();
-  };
-
-  const playText = () => {
-    setTextByWords(textSelected.split(' '));
-    let item = 0;
-    setInterval(() => {
-      setWord(textByWords[item]);
-      item += 1;
-    }, 60000 / wordsByMinute);
-  };
-
-  return (
-    <div className="container">
-      <div>
-        <h1>HITZEZ-HITZ</h1>
-      </div>
-      <section className="text-container">
-        <div className="text">
-          <h2 style={fontSelected}>{word}</h2>
-        </div>
-        <div className="button-container">
-          <button onClick={playText}>PLAY</button>
-          <button onClick={pauseText}>STOP</button>
-        </div>
-      </section>
-      <section className="options">
-        <div className="option-item">
-          <label> Testua: </label>
-          <select value={textSelected} onChange={(e) => setTextSelected(e.target.value)}>
-            {texts.map((text) => (
-              <option key={text.text} value={text.text}>{text.language}</option>
-            ))}
-          </select>
-        </div>
-        <div className="option-item">
-          <label> Letra-tipo: </label>
-          <select value={fontSelected} onChange={(e) => setFontSelected(e.target.value)}>
-            <option value="escolar" style={{ fontFamily: 'escolar', color: 'blue' }}>ESCOLAR</option>
-            <option value="dislexia" style={{ fontFamily: 'dislexia', color: 'green' }}>DISLEXIA</option>
-            <option value="sarakanda" style={{ fontFamily: 'sarakanda', color: 'red' }}>SARAKANDA</option>
-          </select>
-        </div>
-        <div className="option-item">
-          <div className="range">
-            <label> Hitz-min:</label>{wordsByMinute}
-          </div>
-          <input className="slider" type="range" max="120" min="30" step="10" value={wordsByMinute} onChange={(e) => setWordsByMinute(e.target.value)} />
-          <datalist id="tickmarks">
-            <option value="30"></option>
-            <option value="60"></option>
-            <option value="90"></option>
-            <option value="120"></option>
-            <option value="150"></option>
-          </datalist>
-        </div>
-      </section>
-      <textarea
-        value={textSelected}
-        placeholder="Aldatu testua hemen....."
-        style={fontSelected}
-        onChange={(e) => setTextSelected(e.target.value)}
-      />
-    </div>
-  );
+ const pauseText = () => {
+  clearInterval();
 };
 
+const playText = () => {
+  setTextByWords(textSelected.split(' '));
+  let item = 0;
+  setInterval(() => {
+    setWord(textByWords[item]);
+    item += 1;
+  }, 60000 / wordsByMinute);
+};
+
+  return (
+    <>
+      <div className="container">
+        <h1>play word by word</h1>
+      </div>
+        <section className="text-container">
+          {/* <div className="text">
+            <h2 style={fontSelected}>{word}</h2>
+          </div> */}
+          <div className="button-container">
+            <button onClick={playText}>PLAY</button>
+            <button onClick={pauseText}>STOP</button>
+          </div>
+        </section>
+    </>
+  );
+}
 export default WordByWord;
